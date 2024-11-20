4fcbd8216c7423a9e28b0cbee9ca603d36f7cc5c
el-kaboom <sachinchandurkar2493@gmail.com>
2024-11-20 23:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def predict_thyroid_disease():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
